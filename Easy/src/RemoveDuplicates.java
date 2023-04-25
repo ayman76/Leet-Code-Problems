0fa558f4770554f835e508e88435cb996d2c9911
Ayman <a.m_ayman9@outlook.com>
2023-04-25 21:02:00 +0200

    Remove duplicates problem

---
+++ Easy/src/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+public class RemoveDuplicates {
+
+    public static int removeDuplicates(int[] nums) {
+        int k = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
+                nums[k] = nums[i + 1];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        System.out.println(removeDuplicates(arr));
+    }
+
+}
